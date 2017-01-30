fee3406dba61029b3f6091aa9f730dcc4467b1d3
Jason Reeder <info@jasonreeder.com>
2017-01-30 13:50:46 -0800

    Add bonus method

--- employee.rb
+++ employee.rb
@@ -1,10 +1,16 @@
 class Employee
+
+  attr_reader :salary
   
   def initialize(name, title, salary, boss = nil)
     @name = name
     @title = title
     @salary = salary
     @boss = boss
+  end
+
+  def bonus(multiplier)
+    salary * multiplier
   end
 
 end
