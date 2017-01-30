782a6c149f6bf5453207795851faffbd6210b091
Jason Reeder <info@jasonreeder.com>
2017-01-30 14:01:10 -0800

    Add #bonus for Manager

--- manager.rb
+++ manager.rb
@@ -1,8 +1,19 @@
 require_relative 'employee'
 
 class Manager < Employee
+
+  attr_reader :employees
+
   def initialize(name, title, salary, boss = nil, employees = [])
     super(name, title, salary, boss)
     @employees = employees
+  end
+
+  def bonus(multiplier)
+    total = 0
+    employees.each do |employee|
+      total += employee.salary
+    end
+    total * multiplier
   end
 end
