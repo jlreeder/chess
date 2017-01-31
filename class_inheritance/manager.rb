075e7d0212f973c513c0498b708b0c98a9dd9b7e
Jason Reeder <info@jasonreeder.com>
2017-01-30 23:06:24 -0800

    Allow #bonus to calculate recursive for mangagers of managers

--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -18,10 +18,24 @@ class Manager < Employee
   end
 
   def bonus(multiplier)
-    total = 0
-    employees.each do |employee|
-      total += employee.salary
-    end
-    total * multiplier
+    self.total_subsalary * multiplier
   end
+
+  protected
+  # protected: other instances of same class can call, but
+  # nothing else can call
+
+  def total_subsalary
+    total_subsalary = 0
+    self.employees.each do |employee|
+      if employee.is_a?(Manager)
+        total_subsalary += employee.salary + employee.total_subsalary
+      else
+        total_subsalary += employee.salary
+      end
+    end
+
+    total_subsalary
+  end
+
 end
