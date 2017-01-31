bd5d3127ffbc9b03194079a40736830aa0cf31a8
Jason Reeder <info@jasonreeder.com>
2017-01-30 16:50:24 -0800

    Add colorize at cursor position

--- chess/display.rb
+++ chess/display.rb
@@ -9,13 +9,16 @@ class Display
   end
 
   def render
-    @board.board.each do |row|
-      display_row = row.map do |el|
-        el.is_a?(Piece) ? el.value : "-"
+    @board.board.each_with_index do |els, row|
+      display_row = els.map.with_index do |el, col|
+        if el.is_a?(Piece)
+          @cursor.cursor_pos == [row, col] ? el.value.red : el.value
+        else
+          @cursor.cursor_pos == [row, col] ? '-'.red : '-'
+        end
       end
       puts display_row.join(" ")
     end
     nil
   end
-
 end
