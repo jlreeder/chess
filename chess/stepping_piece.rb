2f945b8f862f256e63253302ea55d5779e2dd425
Jason Reeder <info@jasonreeder.com>
2017-01-31 12:02:49 -0800

    Initial commit

---
+++ chess/stepping_piece.rb
@@ -0,0 +1,24 @@
+module SteppingPiece
+
+  def moves
+    move_list = []
+
+    move_dirs.each do |coords|
+      row_mod, col_mod = coords
+      curr_row, curr_col = current_pos
+
+      possible_move = [curr_row + row_mod, curr_col + col_mod]
+      next unless on_board?(possible_move)
+
+      move_list << possible_move
+      end
+
+    end
+
+    move_list
+  end
+
+  def on_board?(pos)
+    pos.all? { |coord| coord.between?(0,7) }
+  end
+end
