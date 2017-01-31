ab9f0eb53957dbb6cf9700b66b7810e2adbdbd89
Jason Reeder <info@jasonreeder.com>
2017-01-31 10:31:55 -0800

    Begin SlidingPiece module

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,5 +1,5 @@
 class Piece
-  attr_reader :value
+  attr_reader :value, :current_pos
 
   def initialize(current_pos)
     @value = "p"
@@ -15,7 +15,8 @@ class Rook < Piece
   include SlidingPiece
 
   def initialize(current_pos)
-    @move_dirs = [[0, 1], [1, 1], [0, -1], [-1, -1]]
+    super
+    @move_dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
   end
 
   protected
--- chess/sliding_piece.rb
+++ chess/sliding_piece.rb
@@ -1,7 +1,15 @@
 module SlidingPiece
 
   def moves
-    # move_dirs.each
+    move_list = []
+
+    move_dirs.each do |coords|
+      curr_row, curr_col = current_pos
+      row_mod, col_mod = coords
+      possible_move = [curr_row + row_mod, curr_col + col_mod]
+    end
+
+    move_list
   end
 
 end
