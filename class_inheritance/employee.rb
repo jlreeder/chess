424340514e799802291f55f7282a758b7f928d89
Jason Reeder <info@jasonreeder.com>
2017-01-30 22:54:07 -0800

    Allow employee to add themselves to boss' employee list

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -1,12 +1,19 @@
 class Employee
 
   attr_reader :salary
-  
+
   def initialize(name, title, salary, boss = nil)
     @name = name
     @title = title
     @salary = salary
+    self.boss = boss
+  end
+
+  def boss=(boss)
     @boss = boss
+    boss.add_employee(self) unless boss.nil?
+
+    boss
   end
 
   def bonus(multiplier)
