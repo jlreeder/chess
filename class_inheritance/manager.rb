ae9e9a862c4463a0a5e95b719714cb435d976208
Jason Reeder <info@jasonreeder.com>
2017-01-30 22:57:37 -0800

    Change default value to allow for better inheritance

--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -4,9 +4,9 @@ class Manager < Employee
 
   attr_reader :employees
 
-  def initialize(name, title, salary, boss = nil, employees = [])
+  def initialize(name, title, salary, boss = nil)
     super(name, title, salary, boss)
-    @employees = employees
+    @employees = []
   end
 
   def bonus(multiplier)
