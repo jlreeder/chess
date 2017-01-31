ab72b252e1b171d8d142a11556e69f071cb865d4
Jason Reeder <info@jasonreeder.com>
2017-01-31 11:02:12 -0800

    Add logic for getting moves

--- chess/sliding_piece.rb
+++ chess/sliding_piece.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 module SlidingPiece
 
   def moves
@@ -7,9 +8,19 @@ module SlidingPiece
       curr_row, curr_col = current_pos
       row_mod, col_mod = coords
       possible_move = [curr_row + row_mod, curr_col + col_mod]
+      while on_board?(possible_move)
+        move_list << possible_move
+
+        new_row, new_col = possible_move
+        row_mod, col_mod = coords
+        possible_move = [new_row + row_mod, new_col + col_mod]
+      end
     end
 
     move_list
   end
 
+  def on_board?(pos)
+    pos.all? { |coord| coord.between?(0,7) }
+  end
 end
