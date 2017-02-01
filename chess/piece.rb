657c119ef92888964f56596051ef56dd7fb9775f
Jason Reeder <info@jasonreeder.com>
2017-01-31 17:51:09 -0800

    Finish logic for Board#in_checkmate?

--- chess/piece.rb
+++ chess/piece.rb
@@ -17,6 +17,19 @@ class Piece
     moves.reject { |move| puts_into_check?(move) }
   end
 
+  def puts_into_check?(move)
+    # make move
+    board[move] = self
+    board[current_pos] = NullPiece.instance
+    # see if in check
+    check = board.in_check?(side)
+    # unmake move
+    board[current_pos] = self
+    board[move] = NullPiece.instance
+
+    check
+  end
+
   def moves
   end
 
