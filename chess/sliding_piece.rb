fd9e09b9731680bc45c5837f48a5a75e750b0cc8
Jason Reeder <info@jasonreeder.com>
2017-01-31 17:29:34 -0800

    Allow sliding piece to take other team's piece

--- chess/sliding_piece.rb
+++ chess/sliding_piece.rb
@@ -10,11 +10,23 @@ module SlidingPiece
 
       loop do
         possible_move = [curr_row + row_mod, curr_col + col_mod]
-        break unless board.on_board?(possible_move) &&
-          !board.piece_there?(possible_move)
+        break unless board.on_board?(possible_move)
 
-        move_list << possible_move
-        curr_row, curr_col = possible_move
+        case board.side_at(possible_move)
+
+        when side
+          break
+
+        when nil
+          move_list << possible_move
+          curr_row, curr_col = possible_move
+
+        else
+          move_list << possible_move
+          break
+
+        end
+
       end
 
     end
